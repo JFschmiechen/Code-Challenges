24d89e7d57656115961ed2846f27172f64508b41
JFschmiechen <jfschmiechen@gmail.com>
2016-10-16 01:58:40 -0700

    Challenge finished
    Implement a function rotateArray(vector<int> arr, int r) which rotates
    the array by r places. Eg 1 2 3 4 5 on being rotated by 2 gives 4 5 1 2
    3.

---
+++ rotateArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeChallenges
+{
+    class Program
+    {
+        static void rotateArray(List<int> list, int r ) // Shifts all values to the right (wrapped)
+        {
+            for (int i = 1; i <= r; i++) // r = amount of rotations
+            {
+
+             list.Insert(0, list.ElementAt(list.Count()-i));
+
+            }
+        }
+        static void Main(string[] args) // Function tester
+        {
+
+            List<int> numbers = new List<int>(5) { 1, 2, 3, 4, 5 }; // Sample
+            rotateArray(numbers, 3);
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine(numbers.ElementAt(i));
+
+            }
+        }
+    }
+}
