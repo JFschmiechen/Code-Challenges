0179793cd8fcd626017a9c99f23dcc5a6846922a
JFschmiechen <jfschmiechen@gmail.com>
2016-10-31 08:20:11 -0700

    cleaned up code

--- calcVolts.cpp
+++ calcVolts.cpp
@@ -18,13 +18,10 @@ void calcVolts(double *curPtr, double *resPtr, double *volPtr) {
 
 int main() {
 	double current[10] = { 10.62, 14.89, 13.21, 16.55, 18.62, 9.47, 6.58, 18.32, 12.15, 3.98 };
-	double *curPtr = current;
 	double resistance[10] = { 4, 8.5, 6, 7.35, 9, 15.3, 3, 5.4, 2.9, 4.8 };
-	double *resPtr = resistance;
 	double voltage[10];
-	double *volPtr = voltage;
 
-	calcVolts(curPtr, resPtr, volPtr);
+	calcVolts(current, resistance, voltage);
 
 	for (int i = 0; i < 10; i++)
 		cout << voltage[i] << endl;
